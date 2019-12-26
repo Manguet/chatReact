5e12266e4cf770cdecf90a7e9bbbab368604bc7e
Manguet <benjamin.manguet@gmail.com>
2019-12-26 17:48:45 +0100

    Quest2 Done

---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Contact.css";
+
+function Contact(props) {
+    return (
+        <div className="Contact">
+            <img src={props.image} alt={props.name} className="avatar"/>
+            <div>
+                <p className="name">{props.name}</p>
+                <div className="status">
+                    <div className={props.online ? 'status-online' : 'status-offline'}/>
+                    {props.isOnline ? "Online" : 'Offline'}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
